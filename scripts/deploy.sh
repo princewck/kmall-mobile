d9995ac40f9596786df29bc16c36c2ada5fd2381
wangchengkai <princewck@gmail.com>
2018-02-04 21:36:45 +0800

    deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-rm -rf ./dist && npm run build && scp -r ./dist/* root@kmall_mobile:/var/www/kmall-mobile && echo "发布到m.quanerdai.com成功！！！"
+rm -rf ./dist && npm run build && rsync -rv --force ./dist/* root@hk:/root/kmall-mobile && echo "发布到m.quanerdai.com成功！！！"
 # rm -rf ./dist && npm run build && rsycn -r -v -v  --delete
